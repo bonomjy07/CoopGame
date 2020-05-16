a6f2168953c3afc00aa4517bc72be2d492ca6bd4
bonomjy07 <bonomjy07@naver.com>
2020-05-16 22:49:40 +0900

    Create explosive actor

---
+++ Source/CoopGame/Private/CoopExplosiveActor.cpp
@@ -0,0 +1,89 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CoopExplosiveActor.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/CoopHealthComponent.h"
+#include "GameFramework/DamageType.h"
+#include "PhysicsEngine/RadialForceComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "DrawDebugHelpers.h"
+
+// Sets default values
+ACoopExplosiveActor::ACoopExplosiveActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	// Create static mesh component
+	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMeshComponent"));
+	StaticMeshComponent->SetSimulatePhysics(true);
+	StaticMeshComponent->SetCollisionObjectType(ECollisionChannel::ECC_PhysicsBody);
+	RootComponent = StaticMeshComponent;
+
+	// Create health component
+	HealthComponent = CreateDefaultSubobject<UCoopHealthComponent>(TEXT("HealthComponent"));
+
+	// Create radical force component
+	RadicalForceComponent = CreateDefaultSubobject<URadialForceComponent>(TEXT("RadicalForceComponent"));
+	RadicalForceComponent->SetupAttachment(StaticMeshComponent);
+	RadicalForceComponent->bIgnoreOwningActor = true;
+	RadicalForceComponent->bAutoActivate = false;
+	RadicalForceComponent->bImpulseVelChange = true;
+	RadicalForceComponent->Radius = 250.f;
+
+	// Set default explosion variables
+	bIsExploded = false;
+
+	// Set default damage varaibles
+	BaseDamage = 20.f;
+	DamageRadius = 250.f;
+}
+
+// Called when the game starts or when spawned
+void ACoopExplosiveActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Set default impulse
+	ExplosionImpulse = StaticMeshComponent->GetMass() * 1000.f;
+	
+	HealthComponent->OnHealthChanged.AddDynamic(this, &ACoopExplosiveActor::OnHealthChanged);
+}
+
+// Called every frame
+void ACoopExplosiveActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ACoopExplosiveActor::OnHealthChanged(UCoopHealthComponent* HealthComp, float Health, float HealthDelta, const UDamageType* DamageType, AController* InstigatedBy, AActor* DamageCauser)
+{
+	if (Health <= 0.0f && !bIsExploded)
+	{
+		// I'm dead
+		bIsExploded = true;
+
+		// Change material explodedMaterial
+		StaticMeshComponent->SetMaterial(0, ExplodedMaterial);
+
+		// Show vfx
+		UGameplayStatics::SpawnEmitterAtLocation(this, ExplosionEffect, GetActorLocation());
+
+		// Blow up
+		FVector BlowUpImpulse = GetActorUpVector() * ExplosionImpulse;
+		StaticMeshComponent->AddImpulse(BlowUpImpulse);
+
+		// Apply damage
+		TArray<AActor*> IgnoreActors; // None
+		UGameplayStatics::ApplyRadialDamage(this, BaseDamage, GetActorLocation(), DamageRadius, UDamageType::StaticClass(), IgnoreActors, DamageCauser, InstigatedBy);
+
+		// Blast away nearby physics actors
+		RadicalForceComponent->FireImpulse();
+
+		//OnExplosion();
+
+		DrawDebugSphere(GetWorld(), GetActorLocation(), DamageRadius, 8, FColor::Yellow, false, 5, 0, 3);
+	}
+}
---
+++ Source/CoopGame/Public/CoopExplosiveActor.h
@@ -0,0 +1,62 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CoopExplosiveActor.generated.h"
+
+UCLASS()
+class COOPGAME_API ACoopExplosiveActor : public AActor
+{
+	GENERATED_BODY()
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	class UStaticMeshComponent* StaticMeshComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	class UCoopHealthComponent* HealthComponent;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	class URadialForceComponent* RadicalForceComponent;
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Explosion")
+	bool bIsExploded;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Explosion")
+	float BaseDamage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Explosion")
+	float DamageRadius;
+
+	/** How much it'll blow up when it explodes */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Explosion")
+	float ExplosionImpulse;
+
+	UPROPERTY(EditDefaultsOnly, Category = "vfx")
+	class UMaterialInterface* ExplodedMaterial;
+
+	UPROPERTY(EditDefaultsOnly, Category = "vfx")
+	class UParticleSystem* ExplosionEffect;
+
+public:	
+	// Sets default values for this actor's properties
+	ACoopExplosiveActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	UFUNCTION()
+	void OnHealthChanged(class UCoopHealthComponent* HealthComp, float Health, float HealthDelta, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser);
+
+//	/** Change the material color when it is exploded */
+//	UFUNCTION(BlueprintImplementableEvent)
+//	void OnExplosion();
+};
